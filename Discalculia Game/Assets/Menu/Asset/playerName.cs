1e735a9bc8790afee73ccb71e9b5e0058f150d13
ahmadsabiq <ahmadsabiqadhy@gmail.com>
2023-12-30 16:38:17 +0700

    Add Error Massage in Input Player Name Scene
    Menambahakan error massage di input player dan mengubah sedikit kodingan playername dan scene untuk error massage

--- Discalculia Game/Assets/Menu/Asset/playerName.cs
+++ Discalculia Game/Assets/Menu/Asset/playerName.cs
@@ -8,11 +8,12 @@ public class playerName : MonoBehaviour
 {
     public string saveName;
     public TMP_Text inputText;
+    public TMP_Text ErrorMessage;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ErrorMessage.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,8 +24,18 @@ public class playerName : MonoBehaviour
 
     public void SetName()
     {
-        saveName = inputText.text;
-        PlayerPrefs.SetString("name", saveName);
-        Debug.Log("Nama " + saveName + " tersimpan");
+        if (inputText.text != null && inputText.text.Length > 0)
+        {
+            saveName = inputText.text;
+            PlayerPrefs.SetString("name", saveName);
+            ErrorMessage.text = "Tersimpan"; // Set the error message
+            ErrorMessage.gameObject.SetActive(true);
+            Debug.Log("Nama " + saveName + " tersimpan");
+        }
+        else
+        {
+            ErrorMessage.text = "Nama Anak harus diisi dan disimpan"; // Set the error message
+            ErrorMessage.gameObject.SetActive(true);
+        }
     }
 }
--- Discalculia Game/Assets/Menu/Asset/scene.cs
+++ Discalculia Game/Assets/Menu/Asset/scene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class scene : MonoBehaviour
 {
@@ -9,11 +10,12 @@ public class scene : MonoBehaviour
     public string saveName;
     public GameObject InputPanel;
     public GameObject MenuPanel;
+    public TMP_Text ErrorMessage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ErrorMessage.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,11 +30,14 @@ public class scene : MonoBehaviour
         {
             InputPanel.SetActive(false);
             MenuPanel.SetActive(true);
+            ErrorMessage.gameObject.SetActive(false);
         }
         else
         {
             InputPanel.SetActive(true);
             MenuPanel.SetActive(false);
+            ErrorMessage.text = "Nama Anak harus diisi dan disimpan"; // Set the error message
+            ErrorMessage.gameObject.SetActive(true);
         }
     }
 }
